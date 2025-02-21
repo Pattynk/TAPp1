a1be5e2e5caa22c680a0b9011316ee18e0594a0d
Pattynk <mtra.irmarivera@gmail.com>
2025-02-20 16:16:23 -0800

    BotonSalir

--- ventana.py
+++ ventana.py
@@ -6,7 +6,7 @@ ventana.title("Mi Primera Ventana")
 ventana.geometry("300x200")  # Tamaño de la ventana
 
 # Crear una etiqueta
-etiqueta = tk.Label(ventana, text="¡Hola, esto es un aviso de bienvenida en commit!", font=("Arial", 12))
+etiqueta = tk.Label(ventana, text="¡Hola, bienvenido!", font=("Arial", 12))
 etiqueta.pack(pady=10)  # Agrega espacio alrededor
 
 # Crear una caja de texto
@@ -16,7 +16,11 @@ entrada.pack(pady=5)
 # Función cuando se presiona el botón
 def mostrar_mensaje():
     texto = entrada.get()
-    etiqueta.config(text=f"Hola, {texto}!")  # Cambia el texto de la 
+    etiqueta.config(text=f"Hola, {texto}!")  
+
+# Función para cerrar la aplicación
+def salir_aplicacion():
+    ventana.quit()  # Cierra la ventana y termina el bucle
 
 # Cambiar el color de fondo de la ventana
     ventana.config(bg="lightblue")  # Puedes cambiar "lightblue" por cualquier color que te guste
@@ -25,6 +29,12 @@ def mostrar_mensaje():
 # Crear un botón
 boton = tk.Button(ventana, text="Saludar", command=mostrar_mensaje)
 boton.pack(pady=10)
+
+
+# Crear un botón de salir
+boton_salir = tk.Button(ventana, text="Salir", command=salir_aplicacion)
+boton_salir.pack(pady=10)
+
 
 # Iniciar el bucle de la aplicación
 ventana.mainloop()
